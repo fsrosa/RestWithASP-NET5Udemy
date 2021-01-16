a55b62be056c98949392d229188aea0c5ca84aba
Fabrício Rosa <fabriciosilvarosa@gmail.com>
2021-01-16 15:07:46 -0300

    creating camades

--- 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/Startup.cs
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/Startup.cs
@@ -8,11 +8,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RestWithASPNETUdemy.Model.Context;
-using RestWithASPNETUdemy.Services.Implementations;
+using RestWithASPNETUdemy.Business.Implementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Repository;
+using RestWithASPNETUdemy.Repository.Implementations;
 
 namespace RestWithASPNETUdemy
 {
@@ -32,9 +35,13 @@ namespace RestWithASPNETUdemy
 
             var connection = Configuration["MySQLConnection:MySQLConnectionString"];
             services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+            // Versionamento de APIs
+            services.AddApiVersioning();
             
             // Dependency Injection
-            services.AddScoped<IPersonService, PersonServiceImplementation>();
+            services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+            services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
