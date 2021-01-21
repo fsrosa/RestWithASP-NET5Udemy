2ab02b588ca18801b11b3e247be7e6636ee8a0fe
Fabrício Rosa <fabriciosilvarosa@gmail.com>
2021-01-20 23:12:17 -0300

    start implementing authentication

---
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/Data/VO/UserVO.cs
@@ -0,0 +1,8 @@
+﻿namespace RestWithASPNETUdemy.Data.VO
+{
+    public class UserVO
+    {
+        public string Password { get; internal set; }
+        public string UserName { get; internal set; }
+    }
+}
